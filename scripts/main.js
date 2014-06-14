35b1bddc8ae3096147e9591be8c0e05c13061a04
Kurt Beheydt <kurt@vliegwerk.be>
2014-06-14 19:57:15 +0200

    multiple units + lock modus: control all units with one buttonclick

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
     console.log('ready to light up!');
+    var lockState = false;
+    var locksliders = $('#locksliders');
 
     $('.slider').slider({
 		value: 201,
@@ -12,26 +14,57 @@ $( document ).ready(function() {
 		}
     });
 
+    locksliders.click(function (e) {
+    	e.preventDefault();
+    	var secondary = $('.secondary');
+    	var modus = $('#modus');
+
+    	if (lockState) {
+    		secondary.show();
+			modus.html('');
+			locksliders.html('start simultane modus');
+    		lockState = false;
+    	} else {
+    		secondary.hide();
+			modus.html('simultane modus actief');
+			locksliders.html('beëindig simultane modus');
+    		lockState = true;
+    	}
+    });
+
     $('.btn').click(function(e) {
 		e.preventDefault();
-		var adres = $(this).parent().attr('data-ip');
+
 		var red = $(this).attr('data-red');
 		var green = $(this).attr('data-green');
 		var blue = $(this).attr('data-blue');
 		var timeout = $(this).parent().attr('data-timeout');
-		var arduinoURL = 'http://' + adres + '/arduino/led/';
 
-		$.ajax({
-			url: arduinoURL + timeout + '/' + red + '/' + green + '/' + blue + '/',
-			dataType: 'jsonp',
-			jsonp: 'jsonp',
-			beforeSend: function( xhr ) {
-				console.log(xhr);
-			},
-			success: function (e) {
-				var response = jQuery.parseJSON(e);
-				console.log(response.status);
-			}
+		var arduinoURLs = [];
+		
+		if (lockState) {
+			$('.unit').each(function() {
+				var adres = $(this).attr('data-ip');
+				arduinoURLs.push('http://' + adres + '/arduino/led/');
+			});
+		} else {
+			var adres = $(this).parent().attr('data-ip');
+			arduinoURLs.push('http://' + adres + '/arduino/led/');
+		}
+
+		$.each( arduinoURLs, function( key, arduinoURL ) {
+			$.ajax({
+				url: arduinoURL + timeout + '/' + red + '/' + green + '/' + blue + '/',
+				dataType: 'jsonp',
+				jsonp: 'jsonp',
+				beforeSend: function( xhr ) {
+					console.log(xhr);
+				},
+				success: function (e) {
+					var response = jQuery.parseJSON(e);
+					console.log(response.status);
+				}
+			});
 		});
     });
 });
